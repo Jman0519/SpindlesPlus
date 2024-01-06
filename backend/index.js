16f2bf4d12b2151efe52550144d20cfa686b4ad1
Jared Herron <jlherron2000@gmail.com>
2024-01-06 13:21:46 -0500

    forgot async

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 server = http.createServer();
 server.listen(80);
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
     console.log('\nIncoming Request!!!');
     const buffers = [];
     for await (const chunk of req) {
@@ -27,6 +27,10 @@ server.on('request', (req, res) => {
     if (url == "/") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(fs.readFileSync('frontend/index.html'));
+    }
+    else if (url == "/newjob.html") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(fs.readFileSync('frontend/newjob.html'));
     }
     else if (url == "/favicon.ico") {
         res.writeHead(200, { 'Content-Type': 'image/x-icon' });
