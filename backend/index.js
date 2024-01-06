d6cdbd5c4be3a541a9bb4bc1bd96932ad70c9e93
Jared Herron <jlherron2000@gmail.com>
2024-01-06 13:00:42 -0500

    removed more game code

--- backend/index.js
+++ backend/index.js
@@ -15,30 +15,6 @@ server.on('request', (req, res) => {
         res.writeHead(200, { 'Content-Type': 'image/x-icon' });
         res.end(fs.readFileSync('images/favicon.ico'));
     }
-    else if (url == '/game.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('frontend/game.html'));
-    }
-    else if (url == '/game.js') {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
-        res.end(fs.readFileSync('frontend/game.js'));
-    }
-    else if (url == "/contactUs.html") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('frontend/contactUs.html'));
-    }
-    else if (url == "/about.html") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('frontend/about.html'));
-    }
-    else if (url == "/privacyPolicy.html") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('frontend/privacyPolicy.html'));
-    }
-    else if (url == '/ads.txt') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(fs.readFileSync('frontend/ads.txt'));
-    }
     else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(fs.readFileSync('frontend/index.html'));
